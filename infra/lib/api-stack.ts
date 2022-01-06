98efaf0f98152fa07be27fa67bc54569e15fea0b
Igor Soroka <igor@soroka.tech>
2022-01-06 09:21:29 +0200

    added first trial lambda

--- infra/lib/api-stack.ts
+++ infra/lib/api-stack.ts
@@ -4,116 +4,36 @@ import {
   aws_dynamodb as ddb,
   StackProps,
   Stack,
-  CfnResource
+  aws_sns as sns,
+  aws_ssm as ssm,
 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {RetentionDays} from "aws-cdk-lib/aws-logs";
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
 export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // const dynamoTable = new ddb.Table(this, 'BookTable', {
-    //   tableName: 'BookStorage',
-    //   readCapacity: 1,
-    //   writeCapacity: 1,
-    //   partitionKey: {
-    //     name: 'id',
-    //     type: ddb.AttributeType.STRING,
-    //   },
-    // });
+    const email = ssm.StringParameter.valueForStringParameter(this, '/billing/email', 1);
 
     // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs-readme.html
     // do not forget to do: npm install --save-dev esbuild@0
     // mayeb next time
 
-    
+    const snsTopic = new sns.Topic(this, 'SkiTopic', {
+      topicName: 'ski-topic',
+    });
 
     const getSkiTrackState = new lambda.Function(this, 'GetSkiTrackHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('../code'),
       handler: 'create.handler',
-      environment: {
-        table: dynamoTable.tableName
-      },
       logRetention: RetentionDays.ONE_WEEK
     });
 
-    // const createBookFunction = new lambda.Function(this, 'CreateHandler', {
-    //   runtime: lambda.Runtime.NODEJS_14_X,
-    //   code: lambda.Code.fromAsset('../code'),
-    //   handler: 'create.handler',
-    //   environment: {
-    //     table: dynamoTable.tableName
-    //   },
-    //   logRetention: RetentionDays.ONE_WEEK
-    // });
-
-    // const getBookFunction = new lambda.Function(this, 'GetHandler', {
-    //   runtime: lambda.Runtime.NODEJS_14_X,
-    //   code: lambda.Code.fromAsset('../code'),
-    //   handler: 'get.handler',
-    //   environment: {
-    //     table: dynamoTable.tableName
-    //   },
-    //   logRetention: RetentionDays.ONE_WEEK
-    // });
-
-    // const listBooksFunction = new lambda.Function(this, 'ListHandler', {
-    //   runtime: lambda.Runtime.NODEJS_14_X,
-    //   code: lambda.Code.fromAsset('../code'),
-    //   handler: 'list.handler',
-    //   environment: {
-    //     table: dynamoTable.tableName
-    //   },
-    //   logRetention: RetentionDays.ONE_WEEK
-    // });
-
-    // const deleteBookFunction = new lambda.Function(this, 'DeleteHandler', {
-    //   runtime: lambda.Runtime.NODEJS_14_X,
-    //   code: lambda.Code.fromAsset('../code'),
-    //   handler: 'delete.handler',
-    //   environment: {
-    //     table: dynamoTable.tableName
-    //   },
-    //   logRetention: RetentionDays.ONE_WEEK
-    // });
-
-    // const updateBookFunction = new lambda.Function(this, 'UpdateHandler', {
-    //   runtime: lambda.Runtime.NODEJS_14_X,
-    //   code: lambda.Code.fromAsset('../code'),
-    //   handler: 'update.handler',
-    //   environment: {
-    //     table: dynamoTable.tableName
-    //   },
-    //   logRetention: RetentionDays.ONE_WEEK
-    // });
-
-    // dynamoTable.grant(createBookFunction, 'dynamodb:CreateItem', 'dynamodb:PutItem')
-    // dynamoTable.grant(getBookFunction, 'dynamodb:GetItem');
-    // dynamoTable.grant(listBooksFunction, 'dynamodb:Scan')
-    // dynamoTable.grant(deleteBookFunction, 'dynamodb:DeleteItem')
-    // dynamoTable.grant(updateBookFunction, 'dynamodb:UpdateItem')
-
-    // const api = new apigw.RestApi(this, `BookAPI`, {
-    //   restApiName: `book-rest-api`,
-    // });
-
-    // const mainPath = api.root.addResource('books');
-
-    // const createBookIntegration = new apigw.LambdaIntegration(createBookFunction);
-    // const getBookIntegration = new apigw.LambdaIntegration(getBookFunction);
-    // const listBooksIntegration = new apigw.LambdaIntegration(listBooksFunction);
-    // const deleteBookIntegration = new apigw.LambdaIntegration(deleteBookFunction);
-    // const updateBookIntegration = new apigw.LambdaIntegration(updateBookFunction);
-
-
-    // mainPath.addMethod('GET', listBooksIntegration);
-    // mainPath.addMethod('POST', createBookIntegration);
-
-    // const idPath = mainPath.addResource('{id}');
-    // idPath.addMethod('GET', getBookIntegration);
-    // idPath.addMethod('DELETE', deleteBookIntegration);
-    // idPath.addMethod('PUT', updateBookIntegration);
+    snsTopic.addSubscription(new EmailSubscription(email, {
+      json: true
+    }));
   }
 }
--- src/functions/getSkiTrackState.ts
+++ src/functions/getSkiTrackState.ts
@@ -1,3 +1,15 @@
+import axios from "axios";
+
 export async function handler () {
-    return 'Hello from lambda!';
+    const url = 'https://api.hel.fi/servicemap/v2/unit/?service=191&only=id,name,location,street_address,services,municipality&include=observations&geometry=false&page_size=1000&municipality=espoo'
+    const resp = await axios.get(url);
+    const data = resp.data as SkiTracksData
+    return {
+        statusCode: 200,
+        body: JSON.stringify(`There are ${data.count} in Espoo`)
+    };
+}
+
+export interface SkiTracksData {
+    count: number;
 }
