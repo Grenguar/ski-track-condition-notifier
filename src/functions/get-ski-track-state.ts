0bf05aba263c90d2b7592854c90d2ef1dfc929bf
Igor Soroka <igor@soroka.tech>
2022-01-08 14:15:28 +0200

    adding new filtering

--- src/functions/get-ski-track-state.ts
+++ src/functions/get-ski-track-state.ts
@@ -3,6 +3,7 @@ import * as AWS from "@aws-sdk/client-sns";
 import { DataResult, SkiTracksData } from "../models/ski-track-data";
 import { PublishCommand, PublishCommandInput } from "@aws-sdk/client-sns";
 import { FinishedTracks } from "../models/finished-tracks";
+import { getOriginalNode } from "typescript";
 
 export async function handler () {
     const url = 'https://api.hel.fi/servicemap/v2/unit/?service=191&only=id,name,location,street_address,services,municipality&include=observations&geometry=false&page_size=1000&municipality=espoo'
@@ -21,9 +22,8 @@ export async function handler () {
     console.log(JSON.stringify(skiData));
     const params: PublishCommandInput = {
         TopicArn: topicArn,
-        Message: JSON.stringify(tracksCount),
-
-        Subject: 'Ski maintenance completed!'
+        Message: JSON.stringify(getLatestMaintenanceData(skiData)),
+        Subject: `Ski maintenance completed! Tracks count: ${tracksCount}`,
     };
     const publishCommand = new PublishCommand(params);
     const snsData = await client.send(publishCommand);
@@ -42,28 +42,31 @@ async function getSkiTrackData() {
 function getLatestMaintenanceData(data: SkiTracksData) {
     return {
         tracks: data.results
+            .filter(filterUnfinished)
             .map((result: DataResult) => {
                 const name = result.name.fi;
                 const address = result.street_address;
-                const skiTrailCondition = result.observations.find(obs => obs.property === 'ski_trail_condition')
-                if (skiTrailCondition) {
-                    const time = skiTrailCondition.time.toString();
-                    if (skiTrailCondition.quality === 'good' && skiTrailCondition.value === 'good') {
-                        const state = 'good';
-                        const finished = true;
-                        return {
-                            finished: finished,
-                            time,
-                            name,
-                            address,
-                            state
-                        }
-                    } else {
-                        return {};
-                    }
-                } else {
-                    return {};
+                const time = result.observations[0].time.toString();
+                return {
+                    finished: true,
+                    time,
+                    name,
+                    address,
+                    state: 'good',
                 }
-            })
+            }) || []
     };  
 }
+
+function filterUnfinished(result: DataResult): boolean {
+    const skiTrailCondition = result.observations.find(obs => obs.property === 'ski_trail_condition')
+    if (skiTrailCondition) {
+        return skiTrailCondition.quality === 'good' && skiTrailCondition.value === 'good';
+    }
+    return false;
+}
+
+// function isFinishedClose(date1: Date, date2: Date, differenceInMins: number) {
+//     const diff = Math.abs(date1 - date2)
+//     return  diff <= differenceInMins*1000;
+// }
